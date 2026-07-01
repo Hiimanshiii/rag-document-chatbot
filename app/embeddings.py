50bc08a4bda21ba212da7befe5d01b416378c7c7
Himanshi <himanshic114@gmail.com>
2026-07-02 01:56:54 +0530

    Use Google Gemini Embeddings API

--- app/embeddings.py
+++ app/embeddings.py
@@ -1,13 +1,17 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
+load_dotenv()
 
 def get_embedding_model():
     """
-    Returns a HuggingFace embedding model.
+    Returns the Google Gemini embedding model.
     """
 
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/gemini-embedding-001",
+        google_api_key=os.getenv("GEMINI_API_KEY")
     )
 
     return embeddings
